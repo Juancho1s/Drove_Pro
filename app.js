0c1104b33abe93de3947766a997656dc6d2aa017
Juancho1s <113451623+Juancho1s@users.noreply.github.com>
2023-07-18 19:50:36 -0600

    this is the update of the app file

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+const session = require("express-session");
+const passport = require('passport');
+const hbs = require('hbs');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
