74b106f1285e547c7953540891f810a1740bce1c
Juancho1s <113451623+Juancho1s@users.noreply.github.com>
2023-08-04 10:56:56 -0600

    comentarization
    There I explained some lines of code

--- controllers/userController.js
+++ controllers/userController.js
@@ -40,14 +40,21 @@ class userController {
 
   /* This method works in order to find a singular user depending on its email */
   static async getUserByEON(req, res) {
+    /* This variable will check if the fields introduced are correct */
     let err = loginValidation.validationResult(req);
+    /* In case they are not the server will return you to the login page */
     if (err) {
       res.redirect("/login");
     }
 
+    /* If every thing is good, these variables will hold the data to use it in some queries */
     let username_email = req.body.username_email;
     let password = req.body.password;
 
+    /*
+      This query will find the user by the user email or the user name and also it will compare if the 
+      passwords match
+    */
     let user = await usersORM.findOne({
       where: {
         [Op.or]: [
@@ -58,12 +65,18 @@ class userController {
       },
     });
 
+    /* 
+    By the end, if the user is a valid one the server will direct you
+    to the home page which will contain all the user folders and files 
+    */
     if (user) {
+      
       res.render("home", {
         title: "DrovePro-Home",
         userID: user.id,
       });
     } else {
+      /* In case that the user is an invalid one, the server will direct you to the login page */
       res.redirect("/login");
     }
   }
