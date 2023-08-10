d30f5836ba49e49a9c1a88164674a7c148cbdcdf
Juancho1s <113451623+Juancho1s@users.noreply.github.com>
2023-08-10 10:48:15 -0600

    login and home update
    I put the interaction with the database in order to recognize the user and fixed a little method(getUserByEON) which give me a user that matches with the already introduced in the login

--- controllers/services/loginValidation.js
+++ controllers/services/loginValidation.js
@@ -4,13 +4,21 @@ const { check, validationResult } = require("express-validator");
 /* These are the validation rules which are going to be executed at the time of adding a new user. */
 const validationRules = [
   /* Checks for the first name to be valid format */
-  check("userName").notEmpty().withMessage("User name is required").trim(),
+  check("userName")
+  .notEmpty()
+  .withMessage("User name is required")
+  .trim(),
 
   /* Checks for the password to be valid format */
-  check("password").notEmpty().withMessage("Password is required"),
+  check("password")
+  .notEmpty()
+  .withMessage("Password is required"),
 
   /* Checks for the first name to be valid format */
-  check("email").trim().isEmail().notEmpty(),
+  check("email")
+  .trim()
+  .isEmail()
+  .notEmpty(),
 ];
 
 module.exports = {
--- models/userORM.js
+++ models/userORM.js
@@ -1,35 +1,37 @@
-const {Sequelize, DataTypes} = require('sequelize');
+const { Sequelize, DataTypes } = require("sequelize");
 
 const sequelize = new Sequelize({
-    host: "localhost",
-    username: "root",
-    password: "123456789",
-    database: "drove",
-    dialect:"mysql",
-    define: {
-        timestamps: false
-    }
+  host: "localhost",
+  username: "root",
+  password: "123456789",
+  database: "drove",
+  dialect: "mysql",
+  define: {
+    timestamps: false,
+  },
 });
 
 const User = sequelize.define("users", {
-    id:{
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true
-    },
-    username:{
-        type: DataTypes.STRING
-    },
-    email:{
-        type: DataTypes.STRING
-    },
-    password:{
-        type: DataTypes.STRING
-    },
-    avatar:{
-        type: DataTypes.STRING
-
-    }
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  username: {
+    type: DataTypes.STRING,
+  },
+  email: {
+    type: DataTypes.STRING,
+  },
+  password: {
+    type: DataTypes.STRING,
+  },
+  avatar: {
+    type: DataTypes.STRING,
+  },
+  filesSharedWith: {
+    type: DataTypes.JSON,
+  },
 });
 
 module.exports = User;
