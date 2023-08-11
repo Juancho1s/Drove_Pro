9e46a6ceb2769a133394ed60c329db410e847976
Juancho1s <113451623+Juancho1s@users.noreply.github.com>
2023-08-11 14:03:06 -0600

    another commit

--- controllers/folderController.js
+++ controllers/folderController.js
@@ -3,6 +3,8 @@ const { Op } = require("sequelize");
 
 
 class folderORM {
+  static async getFolder(req, res){}
+
   static async moveFolderTo(req, res) {}
 
   static async createFolder(req, res) {}
