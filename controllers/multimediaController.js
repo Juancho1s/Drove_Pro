051745ed7992f5dabb21b0a81b77a5cb71868524
Juancho1s <113451623+Juancho1s@users.noreply.github.com>
2023-08-21 09:48:45 -0600

    commit

--- controllers/multimediaController.js
+++ controllers/multimediaController.js
@@ -1,4 +1,7 @@
 const methods = require("./services/methods");
+const fileORM = require("../models/fileORM");
+const folder = require("../models/folderORM");
+
 const { Op } = require("sequelize");
 const crypto = require("crypto-js");
 const multer = require("multer");
@@ -9,42 +12,12 @@ const fs = require("fs");
 class multimediaController {
   /* Get all files and folders in a specific path  */
   static async homeRendering(req, res) {
+    if (req.session.userData.location.length > 1) {
+      let userId = req.session.userData.id;
+      let files = 
+    }else{
 
-    let cryp1 = methods.location(req) + "/movies";
-
-    const encryptionKey = req.session.userData.email;
-    const iv = req.session.userData.password;
-    let cryps = crypto.AES.encrypt(cryp1, encryptionKey, { iv }).toString().replace(/\+/g, '-').replace(/\//g, '_');
-    res.render("home", {
-      title: "Home",
-      stay: true,
-      location: methods.pathEncryption(req),
-      multimedia: {
-        Folder1: {
-          id_user: req.params.id,
-          name: "movies",
-          folderBeforePath: "encryptedUrlSafe",
-          path: cryps,
-        },
-        file: {
-          name: "Quicksort",
-          type: ".pdf",
-          creationDate: "02/10/2023",
-          size: "1000",
-          usersAndPermission: [
-            {
-              name: "Juancho",
-              permission: "read",
-            },
-            {
-              name: "John4",
-              permission: "write",
-            },
-          ],
-          path: "encryptedHex3",
-        },
-      },
-    });
+    }
   }
 }
 
